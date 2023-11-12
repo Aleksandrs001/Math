ea6fdcbe45e1020e5063b3a9f11518e05698f2a7
alexanderbutaev <hinter001@gmail.com>
2023-11-12 23:22:45 +0200

    #007 added translate in LV

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -41,11 +41,15 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <div class="text-gray-400">
-                    <a href="{{ route('change.locale', ['locale' => 'en']) }}">English</a> |
-                    <a href="{{ route('change.locale', ['locale' => 'ru']) }}"> Русский</a>
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <div class="text-gray-400">
+                        <a href="{{ route('change.locale', ['locale' => 'en']) }}" style="{{ app()->getLocale() === 'en' ? 'color: #BDBDBD;' : 'color: #808080;' }}">English</a> |
+                        <a href="{{ route('change.locale', ['locale' => 'ru']) }}" style="{{ app()->getLocale() === 'ru' ? 'color: #BDBDBD;' : 'color: #808080;' }}"> Русский</a> |
+                        <a href="{{ route('change.locale', ['locale' => 'lv']) }}" style="{{ app()->getLocale() === 'lv' ? 'color: #BDBDBD;' : 'color: #808080;' }}">Latviešu</a>
 
+                    </div>
                 </div>
+
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
 
