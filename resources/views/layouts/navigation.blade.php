3e10321d176fe2c7ebc40f382528e1c4e07937b6
alexanderbutaev <hinter001@gmail.com>
2023-11-12 23:24:52 +0200

    #007 added translate in LV

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -45,7 +45,7 @@
                     <div class="text-gray-400">
                         <a href="{{ route('change.locale', ['locale' => 'en']) }}" style="{{ app()->getLocale() === 'en' ? 'color: #BDBDBD;' : 'color: #808080;' }}">English</a> |
                         <a href="{{ route('change.locale', ['locale' => 'ru']) }}" style="{{ app()->getLocale() === 'ru' ? 'color: #BDBDBD;' : 'color: #808080;' }}"> Русский</a> |
-                        <a href="{{ route('change.locale', ['locale' => 'lv']) }}" style="{{ app()->getLocale() === 'lv' ? 'color: #BDBDBD;' : 'color: #808080;' }}">Latviešu</a>
+                        <a href="{{ route('change.locale', ['locale' => 'lv']) }}" style="{{ app()->getLocale() === 'lv' ? 'color: #BDBDBD;' : 'color: #808080;' }}"> Latviešu</a>
 
                     </div>
                 </div>
