b56a997b282afc14ac07db278517445823d2c6a6
alexanderbutaev <hinter001@gmail.com>
2024-02-17 23:29:43 +0200

    minor fix

--- app/Services/StatisticService.php
+++ app/Services/StatisticService.php
@@ -88,7 +88,6 @@ class StatisticService
                         'thisUser' => 'true',
                     ];
                 }
-                break;
             }
         }
         return $sorted;
