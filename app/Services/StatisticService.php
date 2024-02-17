4e543f7405d10eacb47035057145e5b79dfb5dc4
alexanderbutaev <hinter001@gmail.com>
2024-02-17 23:30:22 +0200

    minor fix

--- app/Services/StatisticService.php
+++ app/Services/StatisticService.php
@@ -88,6 +88,7 @@ class StatisticService
                         'thisUser' => 'true',
                     ];
                 }
+                break;
             }
         }
         return $sorted;
