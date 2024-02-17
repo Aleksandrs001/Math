ebd13b453ac5dfac4f35172b26d5698f60e1a117
alexanderbutaev <hinter001@gmail.com>
2024-02-17 17:02:56 +0200

    small fixes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,8 +3,6 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-
-
                 <!-- Navigation Links -->
                 <div class="shrink-0 flex ">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
@@ -15,7 +13,6 @@
 
                     <x-dropdown align="left" width="48">
                         <x-slot name="trigger">
-
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                                 {{ __('messages.plus') }}
                                 <div class="ms-1">
@@ -38,7 +35,6 @@
                     </x-dropdown>
                 </div>
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
-
                     <x-dropdown align="left" width="48">
                         <x-slot name="trigger">
                             <button
@@ -84,8 +80,6 @@
                     </x-nav-link>
                 </div>
             </div>
-
-
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -108,9 +102,7 @@
 
                         <button
                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-
+                            {{ Auth::user()->name }}
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                                      viewBox="0 0 20 20">
