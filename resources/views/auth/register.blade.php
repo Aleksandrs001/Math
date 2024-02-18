d1ad856e8a7b58600a3c0bbe04ce7df0475dc37c
alexanderbutaev <hinter001@gmail.com>
2024-02-18 23:16:12 +0200

    changes text colour to grey

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,7 +25,7 @@
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
 
         <div>
-            <h1 style="color: white;">
+            <h1 style="color: grey;">
                 {{ __('regPage.reg_text') }}
             </h1>
         </div>
