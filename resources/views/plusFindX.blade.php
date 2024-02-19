cb53cfcd91683f6504e719bf50db878293964811
alexanderbutaev <hinter001@gmail.com>
2024-02-19 21:13:14 +0200

    deleted text in header

--- resources/views/plusFindX.blade.php
+++ resources/views/plusFindX.blade.php
@@ -21,7 +21,7 @@
 <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
     @include('layouts.navigation')
     <!-- Page Heading -->
-    <header class="bg-white dark:bg-gray-800 shadow">awd
+    <header class="bg-white dark:bg-gray-800 shadow">
     </header>
     <main>
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
