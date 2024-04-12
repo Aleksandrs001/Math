c173f64ff372a98df1a4f93ba39cecdfcbc61ab8
alexanderbutaev <hinter001@gmail.com>
2024-04-12 20:27:54 +0300

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -874,7 +874,6 @@
 
         <div class="mt-16">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-                {{var_dump('hello world')}}
 {{--                <x-dropdown-link :href="route('about')">--}}
 {{--                    {{ __('messages.about') }}--}}
 {{--                </x-dropdown-link>--}}
