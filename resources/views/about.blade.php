56ec090cdf7b023a03091730206d59f1a7bea88b
alexanderbutaev <hinter001@gmail.com>
2024-04-12 20:46:15 +0300

    about page commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -19,7 +19,7 @@
 </head>
 <body class="font-sans antialiased">
 <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-    @include('layouts.navigation')
+{{--    @include('layouts.navigation')--}}
     <!-- Page Heading -->
     <header class="bg-white dark:bg-gray-800 shadow">
 
