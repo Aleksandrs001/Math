343d449f5aec4f6d48dab2e5700185c8fdb6401c
alexanderbutaev <hinter001@gmail.com>
2026-01-30 22:15:38 +0200

    added google indexation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="index, follow">
     <meta name="google-site-verification" content="googled639664c411f382b">
+    <meta name="google-site-verification" content="7XW-_wB8VVlYGrG5JqWigHi0Agc3urv0pLOvxWuav1Y" />
     <title>Welcome</title>
 
     <!-- Fonts -->
