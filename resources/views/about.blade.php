50e09be4ac29101240b5a9fbc4fb1f9220385fe4
Aleksandrs B <hinter001@gmail.com>
2026-02-02 14:16:28 +0200

    added xml

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,13 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    {{--    <title>{{ config('app.name', 'Laravel') }}</title>--}}
     <title>
-        <title>About — Children Math</title>
+        About — Children Math</title>
     <meta name="description" content="About Children Math — educational platform for learning math.">
     <meta name="robots" content="index, follow">
-    </title>
-
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
